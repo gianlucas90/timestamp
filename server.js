3e06ca38cab9c2133f19531fd9c6e5fe30119157
Gianluca <gianlucazin1990@gmail.com>
2020-11-26 18:27:33 +0100

    exercise completed

--- server.js
+++ server.js
@@ -6,27 +6,44 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/timestamp', function (req, res) {
+  date = new Date();
+  res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+});
 
+app.get('/api/timestamp/:date', function (req, res) {
+  input = req.params.date;
+  if (input.length < 11) {
+    input = Date.parse(input);
+  } else {
+    input = req.params.date * 1;
+  }
+  date = new Date(input);
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const port = 3000;
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
